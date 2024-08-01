7cddc71759c821db25b66a9208cb5fa0522f9dc7
Oleksii <musienko1984@gmail.com>
2024-08-01 23:09:35 +0300

    Task 102. Реорганизация проекта REAgency

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using REAgency.BLL.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddREAgencyContext(connection);
+builder.Services.AddUnitOfWorkService();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
