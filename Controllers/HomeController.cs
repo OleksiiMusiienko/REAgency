bece54037623aeab1478928dd8b59261efce9e94
LevPavelko <lev.pavelko.studies@gmail.com>
2024-08-07 14:36:46 +0200

    Task109: Реализация страницы авторизации и регистрации

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace REAgency.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
