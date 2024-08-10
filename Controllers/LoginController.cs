7c15c1b2d9b3cea1385c9e1ee31be3607d0b16b9
LevPavelko <lev.pavelko.studies@gmail.com>
2024-08-10 15:53:27 +0200

    Task115: Реализация авторизации

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -38,6 +38,18 @@ namespace REAgency.Controllers
                 ModelState.AddModelError("Password", "Пароль повинен містити хоча б одну цифру");
                 return View("Index", reg);
             }
+            ClientDTO clientLogin = await _clientService.GetClientByEmail(reg.RegisterEmail);
+            if (reg.RegisterEmail == clientLogin.Email)
+            {
+                ModelState.AddModelError("RegisterEmail", "Користувач із таким логіном існує");
+                return View("Index", reg);
+            }
+            if(reg.RegisterPassword != reg.ConfirmPassword)
+            {
+                ModelState.AddModelError("ConfirmPassword", "Паролі не збігаються");
+                return View("Index", reg);
+            }
+
             client.Name = reg.RegisterName;
             client.Email = reg.RegisterEmail;
             client.status = true;
@@ -76,6 +88,44 @@ namespace REAgency.Controllers
             return RedirectToAction("Index", "Home");
            
 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(AuthViewModel loginModel)
+        {
+            if(loginModel == null)
+            {
+                ModelState.AddModelError("", "Щось пішло не так");
+                return View("Index", loginModel);
+            }
+
+            var user = await _clientService.GetClientByEmail(loginModel.LoginEmail);
+            if (user == null)
+            {
+                ModelState.AddModelError("LoginPassword", "Не правильний логін або пароль");
+                return View("Index", loginModel);
+            }
+
+            string? salt = user.Salt;
+            byte[] password = Encoding.Unicode.GetBytes(salt + loginModel.LoginPassword);
+
+            byte[] byteHash = SHA256.HashData(password);
+
+            StringBuilder hash = new StringBuilder(byteHash.Length);
+            for (int i = 0; i < byteHash.Length; i++)
+                hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+            if (user.Password != hash.ToString())
+            {
+                ModelState.AddModelError("LoginPassword", "Не правильний логін або пароль");
+                return View("Index", loginModel);
+            }
+
+            HttpContext.Session.SetString("Login", user.Email);
+            HttpContext.Session.SetInt32("Id", user.Id);
+
+             return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
--- Models/AuthViewModel.cs
+++ Models/AuthViewModel.cs
@@ -1,14 +1,27 @@
-﻿namespace REAgency.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REAgency.Models
 {
     public class AuthViewModel
     {
         public string LoginEmail { get; set; }
         public string LoginPassword { get; set; }
 
+        [Required(ErrorMessage = "Поле має бути встановлене")]
         public string RegisterName { get; set; }
+
+        [Required(ErrorMessage = "Поле має бути встановлене")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Не корректно веден Email адрес")]
         public string RegisterEmail { get; set; }
+
+        [Required(ErrorMessage = "Поле має бути встановлене")]
+        [DataType(DataType.Password)]
         public string RegisterPassword { get; set; }
+
+        [Required(ErrorMessage = "Поле має бути встановлене")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
+
     }
 
 }
