c4c86adb6620668505a4491835dcc8d68bd56c83
LevPavelko <lev.pavelko.studies@gmail.com>
2024-10-29 14:10:06 +0100

    Task278: Отображение всех клиентов для сотрудников и админа

---
+++ Models/ClientsPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using REAgency.BLL.DTO.Persons;
+
+namespace REAgency.Models
+{
+    public class ClientsPageViewModel
+    {
+        public IEnumerable<ClientDTO> Clients { get; }
+        public PageViewModel PageViewModel { get; }
+        
+        public ClientsPageViewModel(IEnumerable<ClientDTO> clients, PageViewModel viewModel)
+        {
+            Clients = clients;
+            PageViewModel = viewModel;
+
+        }
+    }
+}
