f85b1b2735e5f985a04f1f5ded49b9a1dec6992c
Виктория <babenko.viktoria.v@gmail.com>
2024-11-06 21:34:01 +0200

    Task 287 Обработчик события кномки удалить с запросом AJAX DeleteObject

---
+++ wwwroot/js/DeleteObject.js
@@ -0,0 +1,21 @@
+﻿
+document.getElementById("myButton").addEventListener("click", function () {
+    
+    id = $(this).data('id');
+
+    fetch('/Office/Delete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(id)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                window.location.assign('/Office/Index');
+                alert("Об'єкт успішно вилучено!");
+            }
+        })
+        .catch(error => console.error('Ошибка:', error));
+});
